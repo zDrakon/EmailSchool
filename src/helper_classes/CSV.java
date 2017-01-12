abc6dc1882da6f00b7274261c4836312adbedd68
EvanYap <evanyap.14@gmail.com>
2017-01-12 00:04:45 -0800

    Restructing CSV to be full 2D array of Strings

--- src/helper_classes/CSV.java
+++ src/helper_classes/CSV.java
@@ -13,19 +13,10 @@ public class CSV {
 		String dataString = FileIO.readFileAsString(filepath);
 		String[] lines = dataString.split("\n");
 
-		int rows = lines.length;
-		int cols = lines[0].length();
+		int rows = lines.length; // Length of the first column
+		int cols = lines[0].length(); // Length of the first row
 
 		this.setData(new String[rows][cols]);
-
-		for (int i = 0; i < lines.length; i++) {
-			String line = lines[i];
-			String[] coords = line.split(",");
-			for (int j = 0; j < cols; j++) {
-				if (coords[j].endsWith("#") || coords[j].endsWith("\n"))
-					coords[j] = coords[j].substring(0, coords[j].length() - 1);
-			}
-		}
 	}
 
 	/***
