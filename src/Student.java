e9f08a78821598789f879a6f79ba4e55f8eba99e
EvanYap <evanyap.14@gmail.com>
2017-01-14 13:28:39 -0800

    restructure student with some getters and setters from Shivanni's addon

--- src/Student.java
+++ src/Student.java
@@ -4,11 +4,35 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int ID;
+	private String desiredClass;
+	private String finalGradeOfPreviousClass;
 
-	public Student(int ID, String firstName, String lastName) {
-		name = firstName + " " + lastName;
-		this.ID = ID;
+	public String getDesiredClass() {
+		return desiredClass;
+	}
 
+	public void setDesiredClass(String desiredClass) {
+		this.desiredClass = desiredClass;
+	}
+
+	public String getFinalGradeOfPreviousClass() {
+		return finalGradeOfPreviousClass;
+	}
+
+	public void setFinalGradeOfPreviousClass(String finalGradeOfPreviousClass) {
+		this.finalGradeOfPreviousClass = finalGradeOfPreviousClass;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
 	}
 
 	public int getID() {
@@ -19,14 +43,22 @@ public class Student {
 		return this.name;
 	}
 
-	public void setID(int ID) {
-		this.ID = ID;
+	public void setID(String ID) {
+		this.ID = Integer.parseInt(ID);
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		setName(this.firstName + this.lastName);
+	}
+
+	public void setLastName(String lastName) {
+		this.firstName = lastName;
+		setName(this.firstName + this.lastName);
+	}
 
 }
